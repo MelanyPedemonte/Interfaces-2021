f13fef22585dbc1543b38642991370275b4da2f3
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2021-09-27 19:30:23 -0300

    Tratando de agregar las fichas

--- entregable-2/js/Piece.js
+++ entregable-2/js/Piece.js
@@ -1,3 +1,57 @@
 "use strict";
 class Piece{
+     //Constructor
+     constructor(x, y, radius, fill, ctx, relleno){
+        this.x = x;
+        this.y = y;
+        this.fill = fill;
+        this.ctx = ctx;
+        this.relleno = relleno;
+        this.radius = radius;
+    }
+
+    //Metodos GET && SET
+    getPosition(x,y){
+        return{
+            x : this.getX(),
+            y : this.getY()
+        };
+    }
+
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    setPosition(x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    getRadius(){
+        return this.radius;
+    }
+
+    setRadius(radius){
+        this.radius = radius;
+    }
+
+    setResaltado(resaltado){
+        this.resaltado =resaltado;
+    }
+
+    //Metodo Especifico
+    draw(){
+        this.ctx.beginPath();
+        this.relleno.onload=function() { //la imagen debe cargarse
+            relleno=cxt1.createPattern(imagen1,"repeat"); //método createPattern
+            cxt1.fillStyle=relleno; //imagen como relleno
+        }
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
 } 
--- entregable-2/js/index.js
+++ entregable-2/js/index.js
@@ -9,9 +9,54 @@ let height = canvas.height;
 let  board = new Image();
 board.src = "./images/board/casillero.png";
 
+//Variables
+let pieces =[];
+
 //Dibuja el tablero
 board.onload = function(){
     let color = "white";
     let tablero = new Board(400, 150, 80, 80, color, ctx, 5);
     tablero.draw();
+    let cantFichas = tablero.getPieces();
+    addPieces(cantFichas);
+}
+
+//Agrego las fichas
+function addPieces(cantFichas){
+    let jugador1 = 0;
+    if(jugador1 <= (cantFichas/2)){
+        addPieces();
+        jugador1 ++;
+    }else{
+        addPieces2();
+    }
+    drawFigure();
+}
+
+function addPieces(){ 
+    let img=new Image(); //iniciar ruta
+    img.src="./images/pieces/principito.png";
+    let color = "white";
+    let p1 = new Piece(30, 150, 33, color, ctx, img);
+    pieces.push(p1);
+}
+
+function addPieces2(){ 
+    let imagen=new Image(); //iniciar ruta
+    imagen.src="./images/pieces/zorro.png";
+    let color = "white";
+    let p2 = new Piece(1000, 150, 23, color, ctx, imagen);
+    pieces.push(p2);
+}
+
+function drawFigure(){
+    clearCanvas();
+    for(let i=0; i<pieces.length; i++){
+        pieces[i].draw();
+    }
+}
+
+function clearCanvas(){
+    ctx.fillStyle = '#F8F8FF';
+    ctx.fillRect(0, 0, width, height);
 }
