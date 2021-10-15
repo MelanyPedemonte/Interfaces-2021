e6e2011322297250a95b9ff9c66cd40f8f243ea6
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2021-10-14 23:20:48 -0300

    Le puse un tope de bajada y subida al personaje dentro del canvas

--- entregable-3/js/bird.js
+++ entregable-3/js/bird.js
@@ -1,23 +1,34 @@
 class Bird {
     constructor(){
+        //Lugar de donde arranca el personaje en pos x e y
         this.x = 150;
         this.y = 200;
-        this.vy = 0; //Velocidad
+        //Velocidad
+        this.vy = 0;
+        //Ancho y alto del personaje
         this.width = 20;
         this.height = 20;
+        //La velocidad con la que cae el personaje
         this.weight = 1;
     }
 
     //Metodo para calcular altura y velocidad
     update(){
+        /*Controla que el personaje no pueda bajar mas que el alto del canvas menos 
+        el alto del personaje x3 */
         if(this.y > canvas.height - (this.height * 3)){
             this.y = canvas.height - (this.height * 3);
             this.vy = 0;
         }else{
+            //Hace que suba el personaje en la velocidad aplicada
             this.vy += this.weight;
             this.y += this.vy;
         }
-
+        //Le pongo un tope hasta donde puede subir
+        if(this.y < 0 + this.height){
+            this.y = this.height + 0;
+        }
+        //Cuando suelto baja en lo que indica flap
         if(spacePressed){
             this.flap();
         }
@@ -28,10 +39,9 @@ class Bird {
     }
 
     draw(){
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = 'pink';
         ctx.fillRect (this.x, this.y, this.width, this.height);
     }
 }
 
-const bird = new Bird();
 
--- entregable-3/js/game.js
+++ entregable-3/js/game.js
@@ -11,16 +11,23 @@ let frame = 0;
 let score = 0; //Puntaje
 let gameSpeed = 2; //Velocidad del juego
 
+//Creamos el personaje
+const bird = new Bird();
+
+
 function animate(){
+    //Limpia el personaje
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //Representa las coordenadas del bird 
+    //Representa las coordenadas del personaje, los saltos y su dibujo
     bird.update();
     bird.draw();
+    //Sirve para el loop de la animacion
     requestAnimationFrame(animate);
 }
-
 animate();
 
+//Hace que al tocar el "espacio" suba
+//https://stackoverflow.com/questions/24386354/execute-js-code-after-pressing-the-spacebar
 window.addEventListener('keydown', (e) => {
     //console.log(e.code);
     if (e.code === "Space") {
@@ -28,6 +35,7 @@ window.addEventListener('keydown', (e) => {
     }
 });
 
+//Hace que al no tocar el "espacio" baje
 window.addEventListener('keyup', (e) => {
     if (e.code === "Space") {
         spacePressed = false;
