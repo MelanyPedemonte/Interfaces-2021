840c53cc86092b84bf314041527970ba11bb8bc8
Melany Pedemonte <melany.pedemonte@coodesoft.com.ar>
2021-10-29 18:58:01 -0300

    estrellas de forma aleatorea

--- entregable-3/js/Game.js
+++ entregable-3/js/Game.js
@@ -16,7 +16,7 @@ class Game{
             if(!this.isOver){
                 if(e.code === "Space"){ 
                    this.bird.fly();
-                   this.score = this.score + 5;      
+                  // this.score = this.score + 5;      
                 };
             };
             let birdDiv = document.getElementById("bird");
@@ -28,22 +28,28 @@ class Game{
                 let star = document.getElementById("star");
                 star.style.display = 'none';
                 this.score = this.score + 100;
-                setTimeout(_=>star.style.display = 'block', 2000);
+                setTimeout(_=>this.starModify(star), 2000);
             };
         });  
     } 
+
+    starModify(star){
+        star.style.display = 'block'; 
+        star.style.top = `${ Math.floor( Math.random() * (70 - 40) + 40 ) }%`;
+    }
+
 
     gameOver(){
         this.isOver = true;
         this.bird.dead();
         let obstacle = document.getElementById("obstacle");
         obstacle.style.display = "none";
+        let star = document.getElementById("star");
+        star.style.display = 'none';
         let puntos = document.getElementById("score");
         puntos.innerHTML = this.score;
         let game = document.getElementById("gameOver");
         game.style.display = "block";
-        let starOver = document.getElementById("star");
-        starOver.style.display = "none";
     }
 
 
