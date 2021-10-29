126caa57c530fdca506ffe5e7c63cfc53f22a010
Melany Pedemonte <melany.pedemonte@coodesoft.com.ar>
2021-10-29 19:49:05 -0300

    inicio

--- entregable-3/js/main.js
+++ entregable-3/js/main.js
@@ -3,3 +3,7 @@ let obstacle = new Obstacle(0,100, 0, 400);
 let star = new Star(300,200);
 let game = new Game(bird, obstacle, star);
 game.initGame();
+setInterval(function(){
+    let inicio = document.getElementById("reglas");
+    inicio.style.display = "none";
+},5000);
