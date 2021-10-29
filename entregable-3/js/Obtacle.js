aa93686a06c56f2b7b44c0a9b6e030d616b3fd72
Melany Pedemonte <melany.pedemonte@coodesoft.com.ar>
2021-10-29 19:23:47 -0300

    detalles en el obstaculo

--- entregable-3/js/Obtacle.js
+++ entregable-3/js/Obtacle.js
@@ -1,16 +1,24 @@
 class Obstacle {
-    constructor(posicionX, posicionY){ 
-        this.div = document.getElementById("obstacle");
+    constructor(posicionX, posicionY, posicionX2, posicionY2){ 
+        this.div = document.getElementById("obstacle-top");
+        this.div2 = document.getElementById("obstacle-bottom");
         this.width = this.div.offsetWidth;
         this.height = this.div.offsetHeight;
+        this.width2 = this.div2.offsetWidth;
+        this.height2 = this.div2.offsetHeight;
         this.posicionX = this.div.style.left = posicionX + "px";
         this.posicionY = this.div.style.top = posicionY + "px";
+        this.posicionX2 = this.div2.style.left = posicionX2 + "px";
+        this.posicionY2 = this.div2.style.top = posicionY2 + "px";
         this.divImage = this.div.style.background = "url('./images/Stone.png') left center";
+        this.divImage2 = this.div2.style.background = "url('./images/Stone.png') left center";
     }
 
     hayColision(x1) {
-        return (x1.offsetLeft < this.div.offsetLeft + this.div.offsetWidth && x1.offsetTop < this.div.offsetTop + this.div.offsetHeight &&
-        x1.offsetLeft + x1.offsetWidth > this.div.offsetLeft && x1.offsetHeight + x1.offsetTop > this.div.offsetTop);
+        return ((x1.offsetLeft < this.div.offsetLeft + this.div.offsetWidth && x1.offsetTop < this.div.offsetTop + this.div.offsetHeight &&
+        x1.offsetLeft + x1.offsetWidth > this.div.offsetLeft && x1.offsetHeight + x1.offsetTop > this.div.offsetTop) 
+        || (x1.offsetLeft < this.div2.offsetLeft + this.div2.offsetWidth && x1.offsetTop < this.div2.offsetTop + this.div2.offsetHeight &&
+            x1.offsetLeft + x1.offsetWidth > this.div2.offsetLeft && x1.offsetHeight + x1.offsetTop > this.div2.offsetTop));
     }  
 
 }
--- entregable-3/js/main.js
+++ entregable-3/js/main.js
@@ -1,5 +1,5 @@
 let bird = new Bird();
-let obstacle = new Obstacle(300,300);
+let obstacle = new Obstacle(0,100, 0, 400);
 let star = new Star(300,200);
 let game = new Game(bird, obstacle, star);
 game.initGame();
